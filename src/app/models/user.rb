6be8ff7f9829b4824098770115adc6566a8b5338
Savvinov Aleksandr <savvinovan@gmail.com>
2019-02-08 17:45:40 +0900

    WIP code refactoring, added methods to User model

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -3,15 +3,20 @@
 class User < ApplicationRecord
 
   def self.get_show(id, geopos)
-    {
-      age: 12,
+    find(id).attributes.merge(
       geopos: {
         latitude: geopos[0][0],
         longitude: geopos[0][1]
       }
-    }
+    ).slice("age", :geopos)
   end
   def self.get_by_geotarget(ids)
-    find(ids).map{ |el| el.attributes.merge(id: el.id, name: el.name, surname: el.surname, gender: el.gender ? "Male" : "Female").slice(:id, :name, :surname, :gender)  }
+    find(ids).map do |el| el.attributes.merge(
+      id: el.id,
+      name: el.name,
+      surname: el.surname,
+      gender: el.gender ? "Male" : "Female"
+    ).slice(:id, :name, :surname, :gender)
+    end
   end
 end
